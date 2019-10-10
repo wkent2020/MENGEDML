0263b1d4019270f4dba6befc6bf2e81320979651
wkent2020 <wkent@uchicago.edu>
2019-10-10 16:01:44 -0500

    A lot of nothing

--- image_read.py
+++ image_read.py
@@ -7,13 +7,20 @@ Created on Thu Oct 10 12:36:06 2019
 """
 
 import cv2
+import numpy as np
+import json
 import os
 
-image_folder = 'norm_imgs'
-filepath = './Old_Groups_Code/H_Spray/150bar/T2'
+#image_folder = 'norm_imgs'
+filepath = './Old_Groups_Code/Honda/80bar'
 
 images = [img for img in os.listdir(filepath)]
 
+print(images[0])
+
 frame = cv2.imread(filepath + '/' + images[0])
 
-cv2.imshow('image', frame)
+print(frame.astype('uint8'))
+
+
+cv2.imwrite(filepath + '/test.tif', frame.astype('uint8'))
