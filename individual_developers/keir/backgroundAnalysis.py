e5617746eb5d369f89294429487dbb8a519de15d
jacksonDougherty <43733459+jacksonDougherty@users.noreply.github.com>
2019-11-12 00:25:58 -0600

    Modify backgroundAnalysis
    I'm not sure where the images in /test_images/ originated. The image
    I've been working with, in my individual developer folder, is identical
    to that found in the normalizedBackgroundRemoved folder in the drive.
    But its shape and pixel values seem to disagree with those from the
    /test_images/ folder, which is weird. I'd probably suggest replacing the
    test_images objects with stuff taken from the drive folder. Also, I
    looked at the old group's code, and it seems to do what it should. It
    calculates a global max and min and scales everything according to
    those values.

--- individual_developers/keir/backgroundAnalysis.py
+++ individual_developers/keir/backgroundAnalysis.py
@@ -2,15 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 #import cousin-directory libraries
-import sys
-sys.path.append("../..")
-from auxiliary_functions.contourFunctions import  *
-from auxiliary_functions.thresholdingFunctions import *
-from kmeans_thresholding.segmentedThresholding import *
-from kmeans_thresholding.iterativeThresholding import *
-from shape_information.Shape import *
-from image_processing.image_filters import *
-from image_processing.cropping.cropImage import *
+
+def cropImage(image, cropTop=0, cropBottom = 0, cropLeft = 0, cropRight =0):
+	"Crop pixels off the image"
+	cropped_image = np.copy(image)
+	cropped_image = cropped_image[cropTop:,cropLeft:]
+	if cropBottom:
+		cropped_image = cropped_image[:-cropBottom,]
+	if cropRight:
+		cropped_image = cropped_image[:,-cropRight]
+	return cropped_image
 
 def processImage(imageDirectory, cropTop = 0, cropLeft = 0, cropRight = 0, cropBottom = 0):
 	img = cv2.imread(imageDirectory,-1)
@@ -33,14 +34,24 @@ def histogram(image):
 	#axs.set_xlim(0,255)
 	plt.show()
 
-backgroundImage = '../../test_images/125.tif'
-#backgroundImage = '../../test_images/20.tif' #pure background
+backgroundImage = 'test_images/120.tif'
+backgroundImage1 = 'individual_developers/jackson/120.tif'
 
+img = cv2.imread(backgroundImage,-1)
+img1 = cv2.imread(backgroundImage1,-1)
+
+print(img[0,0])
+print(img1[0,0])
+print("")
+print(img.shape)
+print(img1.shape)
+#backgroundImage = '../../test_images/20.tif' #pure background
+'''
 backgroundImage = processImage(backgroundImage, cropLeft = 0, cropTop =0, cropBottom = 200, cropRight =420)
 
 plt.imshow(backgroundImage, cmap ='gray')
 plt.show()
 print(np.mean(backgroundImage))
 histogram(backgroundImage)
-
+'''
 
